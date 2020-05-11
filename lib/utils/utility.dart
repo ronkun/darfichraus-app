2c0aa826776a2e86b5b4dd7b09da7f15ddd127dc
ron kun <ronsen.1@gmx.de>
2020-05-11 18:27:49 +0200

    Utility helper class for shared preferences

--- lib/utils/utility.dart
+++ lib/utils/utility.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as Math;
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Utility {
@@ -183,7 +184,66 @@ class Utility {
       width: width,
     ));
   }
+
+  // Future<void> putInSharedPrefText(String prefId, String value) async {            
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   await prefs.setString(prefId, value);
+  // }
+
+  // Future<String> getFromSharedPrefText(String prefId) async {            
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   var value = prefs.getString(prefId);
+  //   return value;
+  // }
 }
+
+class PreferencesHelper {
+  static Future<bool> getBool(String key) async {
+    final p = await prefs;
+    return p.getBool(key) ?? false;
+  }
+
+  static Future setBool(String key, bool value) async {
+    final p = await prefs;
+    return p.setBool(key, value);
+  }
+
+  static Future<int> getInt(String key) async {
+    final p = await prefs;
+    return p.getInt(key) ?? 0;
+  }
+
+  static Future setInt(String key, int value) async {
+    final p = await prefs;
+    return p.setInt(key, value);
+  }
+
+  static Future<String> getString(String key) async {
+    final p = await prefs;
+    return p.getString(key) ?? '';
+  }
+
+  static Future setString(String key, String value) async {
+    final p = await prefs;
+    return p.setString(key, value);
+  }
+
+  static Future<double> getDouble(String key) async {
+    final p = await prefs;
+    return p.getDouble(key) ?? 0.0;
+  }
+
+  static Future setDouble(String key, double value) async {
+    final p = await prefs;
+    return p.setDouble(key, value);
+  }
+
+  // helper
+
+  static Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
+}
+
+
 
 class ConverterUtil {
   static String dateTimeToString(DateTime t) => t.toUtc().toIso8601String();
